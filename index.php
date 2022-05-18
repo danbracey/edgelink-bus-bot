148f86e8a98b727c60e3598c1c02abf6fdb07830
Penguin <penguingeek1@gmail.com>
2022-05-18 14:41:58 +0100

    Only respond to /bus commands

--- index.php
+++ index.php
@@ -19,64 +19,62 @@ echo "Bot is ready!", PHP_EOL;
 
 // Listen for messages.
 $discord->on(Event::INTERACTION_CREATE, function ( Interaction $interaction, Discord $discord) use ($config) {
-    $client = new GuzzleHttp\Client();
-    try {
-        $res = $client->request('GET', 'https://data.bus-data.dft.gov.uk/api/v1/datafeed/?lineRef=EL1&api_key=' . $config['bus_api_key']);
+    if($interaction["data"]["name"] === "bus") {
+        try {
+            $client = new GuzzleHttp\Client();
+            $res = $client->request('GET', 'https://data.bus-data.dft.gov.uk/api/v1/datafeed/?lineRef=EL1&api_key=' . $config['bus_api_key']);
 
-        //Convert XML to JSON
-        $xml = simplexml_load_string($res->getBody());
-        $json = json_encode($xml);
-        $BusData = json_decode($json,TRUE);
-        $lat = $BusData['ServiceDelivery']["VehicleMonitoringDelivery"]["VehicleActivity"]["MonitoredVehicleJourney"]["VehicleLocation"]["Latitude"];
-        $long = $BusData['ServiceDelivery']["VehicleMonitoringDelivery"]["VehicleActivity"]["MonitoredVehicleJourney"]["VehicleLocation"]["Longitude"];
-        $timestamp = strtotime($BusData['ServiceDelivery']["VehicleMonitoringDelivery"]["ValidUntil"]);
-        $valid_date = date('d-m-Y H:i:s', $timestamp);
+            //Convert XML to JSON
+            $xml = simplexml_load_string($res->getBody());
+            $json = json_encode($xml);
+            $BusData = json_decode($json,TRUE);
+            $lat = $BusData['ServiceDelivery']["VehicleMonitoringDelivery"]["VehicleActivity"]["MonitoredVehicleJourney"]["VehicleLocation"]["Latitude"];
+            $long = $BusData['ServiceDelivery']["VehicleMonitoringDelivery"]["VehicleActivity"]["MonitoredVehicleJourney"]["VehicleLocation"]["Longitude"];
+            $timestamp = strtotime($BusData['ServiceDelivery']["VehicleMonitoringDelivery"]["ValidUntil"]);
+            $valid_date = date('d-m-Y H:i:s', $timestamp);
 
-        $embed = new \Discord\Parts\Embed\Embed($discord);
-        $embed->setTitle("EL1 Bus Finder");
-        $embed->setDescription("Location data for the Edge Hill Bus");
-        $embed->setAuthor("Dan Bracey", "https://avatars.githubusercontent.com/u/16801642?v=4", "https://github.com/PenguinNexus");
-        $embed->setColor("#0099ff");
-        $embed->setType("rich");
+            $embed = new \Discord\Parts\Embed\Embed($discord);
+            $embed->setTitle("EL1 Bus Finder");
+            $embed->setDescription("Location data for the Edge Hill Bus");
+            $embed->setAuthor("Dan Bracey", "https://avatars.githubusercontent.com/u/16801642?v=4", "https://github.com/PenguinNexus");
+            $embed->setColor("#0099ff");
+            $embed->setType("rich");
 
-        $embed->addField([
-            'name' => 'Data Valid Until: ',
-            'value' => $valid_date
-        ]);
-        /**$embed->addField([
-            'name' => 'Direction: ',
-            'value' => $BusData['ServiceDelivery']["VehicleMonitoringDelivery"]["VehicleActivity"]["MonitoredVehicleJourney"]["DirectionRef"]
-        ]);**/
-        $embed->addField([
-            'name' => 'Operator Reference: ',
-            'value' => $BusData['ServiceDelivery']["VehicleMonitoringDelivery"]["VehicleActivity"]["MonitoredVehicleJourney"]["OperatorRef"]
-        ]);
-        $embed->addField([
-            'name' => 'Longitude: ',
-            'value' => $long
-        ]);
-        $embed->addField([
-            'name' => 'Latitude: ',
-            'value' => $lat
-        ]);
-        $embed->addField([
-            'name' => 'Vehicle Reference: ',
-            'value' => $BusData['ServiceDelivery']["VehicleMonitoringDelivery"]["VehicleActivity"]["MonitoredVehicleJourney"]["VehicleRef"]
-        ]);
-        $embed->addField([
-            'name' => 'Driver: ',
-            'value' => $BusData['ServiceDelivery']["VehicleMonitoringDelivery"]["VehicleActivity"]["Extensions"]["VehicleJourney"]["DriverRef"]
-        ]);
-        $embed->setImage("https://maps.googleapis.com/maps/api/staticmap?center=53.562528,-2.875634&size=500x400&key=" . $config['map_key'] . "&markers=color:blue%7C" .  $lat . "," . $long . "&zoom=14&path=color:0x0000ff|weight:3|53.567277,%20-2.882428|53.566826,%20-2.882400|53.566851,%20-2.883270|53.566279,%20-2.883316|53.565392,%20-2.882946|53.564111,%20-2.881889|53.558700,%20-2.875358|53.559236,%20-2.874431|53.558788,%20-2.873575|53.558494,%20-2.873682|53.557861,%20-2.872448|53.558369,%20-2.871409|53.557289,%20-2.869295|53.557356,%20-2.869104|53.557308,%20-2.868906|53.557222,%20-2.868847|53.557136,%20-2.868901|53.556779,%20-2.868713|53.556425,%20-2.868917|53.555682,%20-2.870070|53.558654,%20-2.875365|53.564029,%20-2.881800|53.565985,%20-2.883313|53.566853,%20-2.883273|53.567326,%20-2.882811|53.567277,%20-2.882425");
+            $embed->addField([
+                'name' => 'Data Valid Until: ',
+                'value' => $valid_date
+            ]);
+            $embed->addField([
+                'name' => 'Operator Reference: ',
+                'value' => $BusData['ServiceDelivery']["VehicleMonitoringDelivery"]["VehicleActivity"]["MonitoredVehicleJourney"]["OperatorRef"]
+            ]);
+            $embed->addField([
+                'name' => 'Longitude: ',
+                'value' => $long
+            ]);
+            $embed->addField([
+                'name' => 'Latitude: ',
+                'value' => $lat
+            ]);
+            $embed->addField([
+                'name' => 'Vehicle Reference: ',
+                'value' => $BusData['ServiceDelivery']["VehicleMonitoringDelivery"]["VehicleActivity"]["MonitoredVehicleJourney"]["VehicleRef"]
+            ]);
+            $embed->addField([
+                'name' => 'Driver: ',
+                'value' => $BusData['ServiceDelivery']["VehicleMonitoringDelivery"]["VehicleActivity"]["Extensions"]["VehicleJourney"]["DriverRef"]
+            ]);
+            $embed->setImage("https://maps.googleapis.com/maps/api/staticmap?center=53.562528,-2.875634&size=500x400&key=" . $config['map_key'] . "&markers=color:blue%7C" .  $lat . "," . $long . "&zoom=14&path=color:0x0000ff|weight:3|53.567277,%20-2.882428|53.566826,%20-2.882400|53.566851,%20-2.883270|53.566279,%20-2.883316|53.565392,%20-2.882946|53.564111,%20-2.881889|53.558700,%20-2.875358|53.559236,%20-2.874431|53.558788,%20-2.873575|53.558494,%20-2.873682|53.557861,%20-2.872448|53.558369,%20-2.871409|53.557289,%20-2.869295|53.557356,%20-2.869104|53.557308,%20-2.868906|53.557222,%20-2.868847|53.557136,%20-2.868901|53.556779,%20-2.868713|53.556425,%20-2.868917|53.555682,%20-2.870070|53.558654,%20-2.875365|53.564029,%20-2.881800|53.565985,%20-2.883313|53.566853,%20-2.883273|53.567326,%20-2.882811|53.567277,%20-2.882425");
 
-        $interaction->respondWithMessage(MessageBuilder::new()
-            ->setContent("EL1 Bus Data")
-            ->addEmbed($embed)
-        );
-    } catch(Exception $exception) {
-        $interaction->respondWithMessage(MessageBuilder::new()
-            ->setContent("Unable to find Bus! The most common cause of this is that the bus isn't running / has finished for the day, please try again later!")
-        );
+            $interaction->respondWithMessage(MessageBuilder::new()
+                ->setContent("EL1 Bus Data")
+                ->addEmbed($embed)
+            );
+        } catch(Exception $exception) {
+            $interaction->respondWithMessage(MessageBuilder::new()
+                ->setContent("Unable to find Bus! The most common cause of this is that the bus isn't running / has finished for the day, please try again later!")
+            );
+        }
     }
 });
 });
